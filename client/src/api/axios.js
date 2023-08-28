0740ae6e41b9ccd917e207a966164bde5d14af90
Kevin Medina <kevinmedina.dev@gmail.com>
2023-08-28 11:13:47 -0300

    Errores 1
    Corrigiendo errores

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://kevinmedina.vercel.app/', // Cambia esto a la URL de tu servidor en producción
+    baseURL: 'https://kevinmedina.vercel.app/api',
     withCredentials: true,
-});
+})
 
 export default instance;
