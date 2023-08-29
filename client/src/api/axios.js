78b1e62475c888ae1c31a942063fae4ecbf8e628
Kevin Medina <kevinmedina.dev@gmail.com>
2023-08-29 17:20:15 -0300

    Cambios 1

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://kevinmedina.vercel.app',
+    baseURL: 'https://kevinmedina.vercel.app/api',
     withCredentials: true,
 })
 
